c50476a264a8f14be41d35d77c0c311db5c95698
Jan_Hybs <jan.hybs@tul.cz>
2015-09-21 15:12:06 +0200

    Add previous color var, add other color space

--- src/utils/colors.py
+++ src/utils/colors.py
@@ -6,17 +6,20 @@ class Colors(object):
     @staticmethod
     def create(space='normal', size=5):
         if space == 'normal':
-            colors = ['r', 'g', 'b', 'c', 'm', 'y', 'k']
+            colors = ['k', 'r', 'g', 'b', 'c', 'm', 'y', 'k']
             colors.reverse()
 
         if space == 'blue':
-            colors = [(0, 0, float(x) / size) for x in range(1, size+1)]
+            colors = [(0, 0, float(x) / size) for x in range(1, size + 1)]
 
-        if space == 'red':
-            colors = [(float(x) / size, 0, 0) for x in range(1, size+1)]
+        if space == 'red' or space == 'datafilter':
+            colors = [(float(x) / size, 0, 0) for x in range(1, size + 1)]
 
         if space == 'green':
-            colors = [(0, float(x) / size, 0) for x in range(1, size+1)]
+            colors = [(0, float(x) / size, 0) for x in range(1, size + 1)]
+
+        if space == 'datafilter':
+            colors.append('k')
 
         colors = colors * 10
         return Colors(colors)
@@ -24,9 +27,14 @@ class Colors(object):
     def __init__(self, colors):
         self.colors = list(colors)
         self.backup = list(colors)
+        self.previous = None
 
     def reset(self):
         self.colors = list(self.backup)
 
+    def prev(self):
+        return self.previous
+
     def next(self):
-        return self.colors.pop()
+        self.previous = self.colors.pop()
+        return self.previous
