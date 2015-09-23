43fac37836ba2778f44a6cc670456b9f63d60811
Jan_Hybs <jan.hybs@tul.cz>
2015-09-23 15:12:23 +0200

    Add correlation example

--- src/examples.py
+++ src/examples.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # author:   Jan Hybs
-
+import math
 
 import numpy as np
 import utils
+import scipy
 import matplotlib.pyplot as plt
-
-
+#
+#
 # x = np.array([0.0, 1.0, 2.0, 3.0,  4.0,  5.0])
 # y = np.array([0.0, 0.8, 0.9, 0.1, -0.8, -1.0])
 # z = np.polyfit(x, y, 3)
@@ -24,23 +25,49 @@ import matplotlib.pyplot as plt
 # plt.ylim(-2,2)
 # plt.legend()
 # plt.show()
+#
+#
+# n = 1000
+# x = np.range(n) + np.random.random(n)*1000
+# y = np.range(n)
+# x = -np.array([0, 1, 2, 3])
+# y = np.array([-1, 0.2, 0.9, 2.1])
+#
+#
+# A = np.vstack([x, np.ones(len(x))]).T
+# m, c = np.linalg.lstsq(A, y)[0]
+#
+# print "Increasing" if m > 0 else "Decreasing"
+# print np.poly1d((m, c))
+#
+# plt.plot(x, y, 'o', label='Original data', markersize=10)
+# plt.plot(x, m*x + c, 'r', label='Fitted line')
+# plt.legend()
+# plt.show()
 
 
-n = 1000
-x = np.range(n) + np.random.random(n)*1000
-y = np.range(n)
-x = -np.array([0, 1, 2, 3])
-y = np.array([-1, 0.2, 0.9, 2.1])
+y = np.linspace(0, 99, 100) + 5 + np.random.random(100)*100
+x = np.range(100)
 
 
-A = np.vstack([x, np.ones(len(x))]).T
-m, c = np.linalg.lstsq(A, y)[0]
+# correlation
+x_mean = np.mean(x)
+y_mean = np.mean(y)
+x_ = x - x_mean
+y_ = y - y_mean
+#  =
 
-print "Increasing" if m > 0 else "Decreasing"
-print np.poly1d((m, c))
+xy = np.sum (x_ * y_)
+xx = np.sum (x_ * x_)
+yy = np.sum (y_ * y_)
 
-plt.plot(x, y, 'o', label='Original data', markersize=10)
-plt.plot(x, m*x + c, 'r', label='Fitted line')
-plt.legend()
+# Pearson correlation coefficient.
+correlation = xy / math.sqrt(xx * yy)
+
+print scipy
+
+
+
+plt.plot(x, y)
+plt.plot(x_, y_)
 plt.show()
-
