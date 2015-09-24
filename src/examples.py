e63696bd2f9df0faf4a8dcc93f857d19068b9768
Jan_Hybs <jan.hybs@tul.cz>
2015-09-24 12:44:20 +0200

    Add Pearson and Spearman correlation and rank coefficient

--- src/examples.py
+++ src/examples.py
@@ -5,7 +5,7 @@ import math
 
 import numpy as np
 import utils
-import scipy
+import scipy.stats
 import matplotlib.pyplot as plt
 #
 #
@@ -46,28 +46,60 @@ import matplotlib.pyplot as plt
 # plt.show()
 
 
-y = np.linspace(0, 99, 100) + 5 + np.random.random(100)*100
+y = np.linspace(0, 99, 100) + 5 + np.random.random(100) * 100
 x = np.range(100)
 
 
-# correlation
-x_mean = np.mean(x)
-y_mean = np.mean(y)
-x_ = x - x_mean
-y_ = y - y_mean
-#  =
+def pearsonr(x, y):
+    """
+    Pearson product-moment correlation coefficient, measure of the linear correlation between
+        two variables X and Y, giving a value between +1 and −1 inclusive, where 1 is total
+        positive correlation, 0 is no correlation, and −1 is total negative correlation.
+    :param x:
+    :param y:
+    :return:
+    """
+    xm, ym = np.mean(x), np.mean(y)
 
-xy = np.sum (x_ * y_)
-xx = np.sum (x_ * x_)
-yy = np.sum (y_ * y_)
-
-# Pearson correlation coefficient.
-correlation = xy / math.sqrt(xx * yy)
-
-print scipy
+    xn, yn = x - xm, y - ym
+    return np.sum(xn * yn) / math.sqrt(np.sum(xn * xn) * np.sum(yn * yn))
 
 
+def spearmanr(x, y):
+    """
+    Spearman's rank correlation coefficient, nonparametric measure of statistical dependence between
+        two variables. It assesses how well the relationship between two variables can be
+        described using a monotonic function. If there are no repeated data values,
+        a perfect Spearman correlation of +1 or −1 occurs when each of the variables
+        is a perfect monotone function of the other.
+    :param x:
+    :param y:
+    :return:
+    """
+    n = len(x)
+    xi = np.argsort(x)
+    yi = np.argsort(y)
+    yii = np.argsort(yi)
 
-plt.plot(x, y)
-plt.plot(x_, y_)
+    # xs = np.array([x[i] for i in xi])
+    # ys = np.array([y[i] for i in xi])
+    # ysy = np.array([y[i] for i in yi])
+
+    xr = np.range(n) + 1
+    yr = np.array([yii[i] for i in xi]) + 1
+    dr = (xr - yr) * (xr - yr)
+    ds = np.sum(dr)
+
+    return 1 - (6.0 * ds) / (n * (n * n - 1.0))
+
+
+x = np.array([106, 86, 100, 101, 99, 103, 97, 113, 112, 110])
+y = np.array([  7,  0,  27,  50, 28,  29, 20,  12,   6,  17])
+
+
+print scipy.stats.stats.spearmanr(x, y)
+print spearmanr (x, y)
+print pearsonr(x, y)
+
+plt.scatter(x, y)
 plt.show()
