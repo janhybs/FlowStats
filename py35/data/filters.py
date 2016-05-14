f64683dedc3cb9da067b3eb77f60fbaf9a20e4d9
Jan Hybš <jan.hybs@tul.cz>
2016-05-14 21:41:03 +0200

    Add python 3.5 version of experiment

---
+++ py35/data/filters.py
@@ -0,0 +1,172 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# author:   Jan Hybs
+
+import numpy as np
+weighted_hamming = np.hamming(10) / np.sum(np.hamming(10))
+
+
+def sma_filter(x, y, l=11, trim=False):
+    """
+    Moving average filter of length l
+    :param x:
+    :param y:
+    :param l:
+    :param trim:
+    :return:
+    """
+    n = len(y)
+    Y = np.zeros(n)
+    for i in range(0, n):
+        Y[i] = np.sum(y[i:(i + l)])
+    Y /= l
+
+    if trim:
+        return x[:-l], Y[:-l]
+    return x, Y
+
+
+def maww_filter(x, y, window=weighted_hamming, causal=True):
+    """
+    Moving average with window
+    :param y: input data
+    :param window: windows which will be used to filter data
+    :param causal:
+    :return:filtered data
+    """
+    n = len(y)
+    l = len(window)
+    if l == 0:
+        return y[:]
+
+    # causal filter
+    if causal:
+        xc = np.append(y[:], np.zeros(l))
+        Y = np.zeros(n)
+        for i in range(0, n):
+            Y[i] = np.sum(np.multiply(xc[i:i + l], window))
+        return x, Y
+
+    # # non-causal filter
+    if not causal:
+        lp = l / 2
+        xc = np.append(np.zeros(lp), y[:])
+        xc = np.append(xc, np.zeros(l - lp))
+        Y = np.zeros(n)
+        for i in range(lp, n + lp):
+            # print len (xc[i - lp:i + l - lp]), i - lp, i + l - lp
+            Y[i - lp] = np.sum(np.multiply(xc[i - l / 2:i + l - l / 2], window))
+
+        return x, Y
+
+
+def ewma_filter(x, y, lam=0.1, L=3):
+    """
+    Exponentially weighted moving average
+    :param y: input data
+    :param lam: λ, the weight given to the most recent rational subgroup mean
+    :param L: L, the multiple of the rational subgroup standard deviation that establishes the control limits. L is typically set at 3 to match other control charts, but it may be necessary to reduce L slightly for small values of λ
+    :return: observations z_i
+    """
+    n = len(y)
+    Y = np.zeros(n)
+
+    Y[0] = y[0]
+    for i in range(1, n):
+        Y[i] = lam * y[i] + (1 - lam) * Y[i - 1]
+    return x, Y
+
+
+def ewma_filter_pandas(x, y, span=19):
+    """
+    Exponentially weighted moving average
+    :param y: input data
+    :param span: center of mass (λ) is calculated as lamda λ = 2/(span + 1), the weight given to the most recent rational subgroup mean
+    :return: observations z_i
+    """
+    import pandas
+
+    return x, pandas.ewma(y, span=span)
+
+
+
+def ewma_variance(x, y, lam=0.2, L=3):
+    """
+    Exponentially weighted moving average control chart
+    :param y: input data
+    :param lam: λ, the weight given to the most recent rational subgroup mean
+    :param L: L, the multiple of the rational subgroup standard deviation that establishes the control limits. L is typically set at 3 to match other control charts, but it may be necessary to reduce L slightly for small values of λ
+    :return:
+    """
+    n = len(y)
+    mean = np.mean(y)
+    sigma = np.math.sqrt(np.var(y))
+    s = np.math.sqrt(lam / (2 - lam))
+    ucl = np.ones(n) * (mean + L * s * sigma)
+    lcl = np.ones(n) * (mean - L * s * sigma)
+
+    return (x, ucl), (x, lcl)
+
+
+def ewma_adaptive_variance_linear(x, y, lam=0.5, L=3, l=50):
+    """
+    Exponentially weighted moving average adaptive control chart
+    :param y: input data
+    :param lam: λ, the weight given to the most recent rational subgroup mean
+    :param L: L, the multiple of the rational subgroup standard deviation that establishes the control limits. L is typically set at 3 to match other control charts, but it may be necessary to reduce L slightly for small values of λ
+    :return:
+    """
+    n = len(y)
+    ucl = np.zeros(n)
+    lcl = np.zeros(n)
+
+    i = 0
+    ucl[i-1] = np.max(y[0:25])
+    lcl[i-1] = np.min(y[0:25])
+    s = np.math.sqrt(lam / (2 - lam))
+    for j in range(l, n + l, l):
+        mean = np.mean(y[i:j])
+        sigma = np.math.sqrt(np.var(y[i:j]))
+        ucl_val = mean + L * s * sigma
+        lcl_val = mean - L * s * sigma
+
+        ucl[i:j] = np.linspace(ucl[i-1], ucl_val, l)
+        lcl[i:j] = np.linspace(lcl[i-1], lcl_val, l)
+        i = j
+
+    # ucl = ewmap(ucl)
+    # lcl = ewmap(lcl)
+
+    return (x, ucl), (x, lcl)
+
+
+def des_filter(x, y, alpha=0.1, beta=0.15):
+    """
+    Double exponential smoothing
+    :param y:
+    :param alpha: α
+    :param beta: β
+    :return:
+    """
+    n = len(y)
+    Y = np.zeros(n)
+    b = np.zeros(n)
+
+    # first step al
+    Y[0] = y[0]
+    b[0] = y[1] - y[0]
+    b[0] = 0
+
+    for i in range(1, n):
+        Y[i] = alpha * y[i] + (1 - alpha) * (Y[i - 1] + b[i - 1])
+        b[i] = beta * (Y[i] - Y[i - 1]) + (1 - beta) * b[i - 1]
+
+    return x, Y
+
+
+def combine_filter(*functions):
+    def combine(x, y):
+        for function in functions:
+            x, y = function(x, y)
+        return x, y
+    return combine
---
+++ py35/data/generator.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# author:   Jan Hybs
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from data import filters
+
+
+class DataConfig(object):
+    def __init__(self, size, generator, start=0):
+        self.size = size
+        self.generator = generator
+        self.start = start
+
+
+def constant_generator(mu=0.0, sigma=1.0):
+    def generator(i):
+        return np.random.normal(mu, sigma)
+    return generator
+
+
+def linear_generator(scale=1.0, mu=0.0, sigma=1.0):
+    def generator(i):
+        return i * scale + np.random.normal(mu, sigma)
+    return generator
+
+
+def exponential_generator(scale=1.0, power=2, mu=0.0, sigma=1.0):
+    def generator(i):
+        return (i ** power) * scale + np.random.normal(mu, sigma)
+    return generator
+
+
+def generate(configs):
+    """
+    :rtype: numpy.core.multiarray.ndarray
+    :type configs: list[DataConfig]
+    """
+    data = []
+    for config in configs:
+        config_data = [config.generator(i) for i in range(config.start, config.start + config.size)]
+        data.extend(config_data)
+
+    return np.array(range(len(data)), dtype=np.float32), np.array(data, dtype=np.float32)
+
+
+def plot(data, color, method='scatter', **kwargs):
+    getattr(plt, method)(*data, c=color, **kwargs)
+
+
+scatter_settings = dict(edgecolors='none')
+data = generate(
+    [
+        DataConfig(300, constant_generator(sigma=0.5)),
+        DataConfig(300, constant_generator(sigma=2.0)),
+        DataConfig(300, exponential_generator(scale=0.0001)),
+        DataConfig(300, linear_generator(scale=0.01), start=int(300*300*0.01))
+    ]
+)
+
+raw_data = data
+sma_data = filters.sma_filter(*data)
+maww_data = filters.maww_filter(*data)
+ewma_data = filters.ewma_filter(*data)
+des_data = filters.des_filter(*data)
+
+
+# plot data and filtered data
+plot(raw_data,  'blue',     'scatter', **scatter_settings)
+plot(sma_data,  'red',      'plot')
+plot(maww_data, 'green',    'plot')
+plot(ewma_data, 'black',    'plot')
+plot(des_data,  'brown',    'plot')
+
+# combining filters
+des_ewma = filters.combine_filter(filters.des_filter, filters.ewma_filter)
+plot(des_ewma(*data), 'pink', 'scatter')
+
+# plot control chart
+ucl, lcl = filters.ewma_variance(*data)
+plot(ucl, 'purple', 'plot')
+plot(lcl, 'purple', 'plot')
+
+# dynamic controls
+ducl, dlcl = filters.ewma_adaptive_variance_linear(*data)
+plot(ducl, 'orange', 'plot')
+plot(dlcl, 'orange', 'plot')
+
+plt.show()
+
